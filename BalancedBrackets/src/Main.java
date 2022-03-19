bbb27156f7d56ad07635af837d84e5f33d64460c
Ashutosh <ashutoshkumarpandey183@gmail.com>
2022-03-19 21:20:27 +0530

    check if brackets are balanced

---
+++ BalancedBrackets/src/Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        Stack<Character>st = new Stack<>();
+        String str = scn.nextLine();
+        for(int i=0; i<str.length(); ++i){\
+
+    }
+}
